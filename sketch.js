36a3cbe8a8e1b51e8cdb3ffefea91d8c785c663a
Laura-Lee Hollande <92219209+Laura-LeeHollande@users.noreply.github.com>
2024-07-30 00:59:21 +0200

    Update sketch.js
    Final code

--- sketch.js
+++ sketch.js
@@ -17,7 +17,6 @@ let btn_bottom = [];
 
 function setup() {
   canvas = createCanvas(weight, height);
-
   /* ------------------------------- Load Songs ------------------------------- */
   arcadeRetroSong = loadSound('arcadeRetroSong.wav');
   barking = loadSound('barking.wav');
@@ -29,17 +28,19 @@ function setup() {
   transition = loadSound('transition.wav');
   whoosh = loadSound('whoosh.wav');
 
-  let b1 = new Button(weight / 3, height / 3, 200, 80, color(226, 132, 19), color(244, 190, 124), arcadeRetroSong);
-  let b2 = new Button(weight / 2, height / 3, 200, 80, color(0, 159, 183), color(153, 241, 255), barking);
-  let b3 = new Button(2 * weight / 3, height / 3, 200, 80, color(145, 145, 233), color(204, 204, 245), fastPunch);
+  let spacing = 20;
+
+  let b1 = new Button(weight / 3, height / 3 - spacing, 200, 80, color(226, 132, 19), color(244, 190, 124), arcadeRetroSong);
+  let b2 = new Button(weight / 2, height / 3 - spacing, 200, 80, color(0, 159, 183), color(153, 241, 255), barking);
+  let b3 = new Button(2 * weight / 3, height / 3 - spacing, 200, 80, color(145, 145, 233), color(204, 204, 245), fastPunch);
 
   let b4 = new Button(weight / 3, height / 2, 200, 80, color(205, 92, 92), color(240, 128, 128), fastTransition);
   let b5 = new Button(weight / 2, height / 2, 200, 80, color(72, 209, 204), color(175, 238, 238), retroNotif);
   let b6 = new Button(2 * weight / 3, height / 2, 200, 80, color(255, 215, 0), color(255, 255, 0), roar);
 
-  let b7 = new Button(weight / 3, 2 * height / 3, 200, 80, color(255, 163, 175), color(255, 214, 220), toyWhistle);
-  let b8 = new Button(weight / 2, 2 * height / 3, 200, 80, color(143, 45, 86), color(216, 131, 166), transition);
-  let b9 = new Button(2 * weight / 3, 2 * height / 3, 200, 80, color(236, 78, 32), color(234, 162, 133), whoosh);
+  let b7 = new Button(weight / 3, 2 * height / 3 + spacing, 200, 80, color(255, 163, 175), color(255, 214, 220), toyWhistle);
+  let b8 = new Button(weight / 2, 2 * height / 3 + spacing, 200, 80, color(143, 45, 86), color(216, 131, 166), transition);
+  let b9 = new Button(2 * weight / 3, 2 * height / 3 + spacing, 200, 80, color(236, 78, 32), color(234, 162, 133), whoosh);
 
   btn_top.push(b1);
   btn_top.push(b2);
@@ -64,21 +65,38 @@ function draw() {
   }
 }
 
+function mousePressed() {
+  for (let i = 0; i < btn_top.length; i++) {
+    btn_top[i].clicked(mouseX, mouseY);
+    btn_middle[i].clicked(mouseX, mouseY);
+    btn_bottom[i].clicked(mouseX, mouseY);
+  }
+}
+
+function mouseReleased() {
+  let spacing = 20;
+  for (let i = 0; i < btn_top.length; i++) {
+    btn_top[i].y = height / 3 - spacing;
+    btn_middle[i].y = height / 2;
+    btn_bottom[i].y = 2 * height / 3 + spacing;
+  }
+}
+
 class Button {
-  constructor(x, y, w, h, color, accent, song) {
+  constructor(x, y, w, h, color, accent, sound) {
     this.x = x;
     this.y = y;
     this.w= w;
     this.h = h;
     this.color = color;
     this.accent = accent;
-    this.song = song;
+    this.sound = sound;
   }
 
   show()  {
     noStroke();
     fill(this.color);
-    rect((this.x - 100), this.y, this.w, this.h);
+    rect((this.x - 100), this.y, this.w, 50);
 
     fill(this.accent);
     ellipse(this.x, this.y, this.w, this.h);
